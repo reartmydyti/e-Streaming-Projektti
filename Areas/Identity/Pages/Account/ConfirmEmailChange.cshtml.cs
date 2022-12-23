a01c947728d76f7ce93aac2010f687b63f960ac7
reartmydyti <104927857+reartmydyti@users.noreply.github.com>
2022-12-23 16:04:22 +0100

    Update ConfirmEmailChange.cshtml.cs

--- Areas/Identity/Pages/Account/ConfirmEmailChange.cshtml.cs
+++ Areas/Identity/Pages/Account/ConfirmEmailChange.cshtml.cs
@@ -47,8 +47,7 @@ namespace MovieDB.Areas.Identity.Pages.Account
                 return Page();
             }
 
-            // In our UI email and user name are one and the same, so when we update the email
-            // we need to update the user name.
+            
             var setUserNameResult = await _userManager.SetUserNameAsync(user, email);
             if (!setUserNameResult.Succeeded)
             {
