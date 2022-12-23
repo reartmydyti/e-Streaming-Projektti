54da689a8712bd5946c04a7c980a5e35b957d8be
reartmydyti <104927857+reartmydyti@users.noreply.github.com>
2022-12-23 16:13:34 +0100

    Update ResetPassword.cshtml.cs

--- Areas/Identity/Pages/Account/ResetPassword.cshtml.cs
+++ Areas/Identity/Pages/Account/ResetPassword.cshtml.cs
@@ -70,7 +70,7 @@ namespace MovieDB.Areas.Identity.Pages.Account
             var user = await _userManager.FindByEmailAsync(Input.Email);
             if (user == null)
             {
-                // Don't reveal that the user does not exist
+                
                 return RedirectToPage("./ResetPasswordConfirmation");
             }
 
