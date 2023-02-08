ebefcd8286e25983214c4c352fb37c886bc4bd97
ibergocaj <93524820+ibergocaj@users.noreply.github.com>
2023-02-08 21:10:27 +0100

    Add files via upload

---
+++ Areas/Admin/Controllers/NacionalitetiController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MovieDB.Data;
+using MovieDB.Models;
+
+namespace MovieDB.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class NacionalitetiController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public NacionalitetiController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Nacionaliteti
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Nacionaliteti.ToListAsync());
+        }
+
+        // GET: Admin/Nacionaliteti/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var nacionaliteti = await _context.Nacionaliteti
+                .FirstOrDefaultAsync(m => m.Nacionaliteti_ID == id);
+            if (nacionaliteti == null)
+            {
+                return NotFound();
+            }
+
+            return View(nacionaliteti);
+        }
+
+        // GET: Admin/Nacionaliteti/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Nacionaliteti/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Nacionaliteti_ID,Nacionaliteti_Name")] Nacionaliteti nacionaliteti)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(nacionaliteti);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(nacionaliteti);
+        }
+
+        // GET: Admin/Nacionaliteti/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var nacionaliteti = await _context.Nacionaliteti.FindAsync(id);
+            if (nacionaliteti == null)
+            {
+                return NotFound();
+            }
+            return View(nacionaliteti);
+        }
+
+        // POST: Admin/Nacionaliteti/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Nacionaliteti_ID,Nacionaliteti_Name")] Nacionaliteti nacionaliteti)
+        {
+            if (id != nacionaliteti.Nacionaliteti_ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(nacionaliteti);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!NacionalitetiExists(nacionaliteti.Nacionaliteti_ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(nacionaliteti);
+        }
+
+        // GET: Admin/Nacionaliteti/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var nacionaliteti = await _context.Nacionaliteti
+                .FirstOrDefaultAsync(m => m.Nacionaliteti_ID == id);
+            if (nacionaliteti == null)
+            {
+                return NotFound();
+            }
+
+            return View(nacionaliteti);
+        }
+
+        // POST: Admin/Nacionaliteti/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var nacionaliteti = await _context.Nacionaliteti.FindAsync(id);
+            _context.Nacionaliteti.Remove(nacionaliteti);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool NacionalitetiExists(int id)
+        {
+            return _context.Nacionaliteti.Any(e => e.Nacionaliteti_ID == id);
+        }
+    }
+}
---
+++ Areas/Admin/Controllers/ReleasesController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MovieDB.Data;
+using MovieDB.Models;
+
+namespace MovieDB.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ReleasesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReleasesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Releases
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Release.ToListAsync());
+        }
+
+        // GET: Admin/Releases/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var release = await _context.Release
+                .FirstOrDefaultAsync(m => m.Release_ID == id);
+            if (release == null)
+            {
+                return NotFound();
+            }
+
+            return View(release);
+        }
+
+        // GET: Admin/Releases/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Releases/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Release_ID,Release_Name")] Release release)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(release);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(release);
+        }
+
+        // GET: Admin/Releases/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var release = await _context.Release.FindAsync(id);
+            if (release == null)
+            {
+                return NotFound();
+            }
+            return View(release);
+        }
+
+        // POST: Admin/Releases/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Release_ID,Release_Name")] Release release)
+        {
+            if (id != release.Release_ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(release);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ReleaseExists(release.Release_ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(release);
+        }
+
+        // GET: Admin/Releases/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var release = await _context.Release
+                .FirstOrDefaultAsync(m => m.Release_ID == id);
+            if (release == null)
+            {
+                return NotFound();
+            }
+
+            return View(release);
+        }
+
+        // POST: Admin/Releases/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var release = await _context.Release.FindAsync(id);
+            _context.Release.Remove(release);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ReleaseExists(int id)
+        {
+            return _context.Release.Any(e => e.Release_ID == id);
+        }
+    }
+}
